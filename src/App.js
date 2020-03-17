6b1df4cd358401c4210486a7d093ce9c2c50e655
ImNath <moorene@jay.washjeff.edu>
2020-03-16 20:23:07 -0400

    WIP props system

--- src/App.js
+++ src/App.js
@@ -2,25 +2,57 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {color: null};
+
+    this.setColorChoice = this.setColorChoice.bind(this);
+    this.setSizeChoice = this.setSizeChoice.bind(this);
+    this.setBirdChoice = this.setBirdChoice.bind(this);
+  }
+  
+  setColorChoice(colorChoice){
+    this.setState({color : colorChoice});
+  }
+
+  setSizeChoice(sizeChoice){
+    this.setState({size : sizeChoice});
+  }
+
+  setBirdChoice(birdChoice){
+    this.setState({bird : birdChoice});
+  }
+  
+  componentDidMount(){
+    this.setColorChoice('red');
+    this.setSizeChoice('small');
+    this.setBirdChoice('robin');
+    this.setColorChoice('blue');
+  }
+
+  
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload NEW.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
