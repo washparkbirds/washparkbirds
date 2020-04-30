aeed45c5fae05b4c163fe9826e781e1bdb92ac77
ImNath <moorene@jay.washjeff.edu>
2020-04-30 18:45:55 -0400

    added about page

--- src/RouterComponents/PageOfAbout.js
+++ src/RouterComponents/PageOfAbout.js
@@ -5,7 +5,14 @@ import { Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles';
-import TopAppBar from '../components/TopAppBar'
+import TopAppBar from '../components/TopAppBar';
+import Paper from '@material-ui/core/Paper';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import WebIcon from '@material-ui/icons/Web';
+import IconButton from '@material-ui/core/IconButton'
+
+
 
 
 
@@ -16,6 +23,30 @@ function PageOfAbout(props) {
         bufferBar:{
             paddingTop: 40,
         },
+        logo:{
+            maxWidth: '250px',
+            marginBottom: 40,
+        },
+
+        aboutPaper:{
+            maxWidth: '70%',
+            margin: '0 auto',
+            marginTop: 40,
+            padding: 40,
+            textAlign: 'left',
+
+        },
+        grid:{
+            marginTop: 40,
+            display: 'flex',
+            flexDirection: 'column',
+            textAlign: 'left',
+
+        },
+
+        learnMore:{
+            textWeight: 'bold',
+        }
 
     }));
 
@@ -26,8 +57,40 @@ function PageOfAbout(props) {
     return (
         <React.Fragment>
             <TopAppBar title="About Us"></TopAppBar>
-           
-        </React.Fragment>
+            <Paper className={classes.aboutPaper}elevation={3}>
+
+                <Grid container direction="column" justify="space-evenly" alignItems="center">
+                    <Grid item xs={12} sm={12} lg={12}>
+                    <img className={classes.logo}src={"/images/WashPA Outdoors Co.png"} alt="Bird" ></img>
+                    </Grid>
+                    <Grid item xs={12} sm={12} lg={12}>
+                        <Typography>
+                            WashPark Birds is a beginner-friendly bird identification application for Washington, PA's parks.
+                            With the application, you can explore to see the many types of birds that visit Washington park!
+                            The app was made for WashPA Outdoors Co., an organization dedicated to preserving Washington PA's local parks
+                            by engaging the community with events, programming, and education.
+                        </Typography>
+                    </Grid>
+                    <Grid className={classes.grid} item xs={12} sm={12} lg={12}>
+                        <Typography>
+                        To learn more about WashPA Outdoors Co. click on any of the images below.<br></br><br></br>
+                        </Typography>
+
+                            <IconButton onClick={() => {window.location.href = 'https://www.facebook.com/washpaoutdoors/';}} edge="start" className={classes.menuButton} color="inherit" aria-label="About Us">
+                                <FacebookIcon>Facebook</FacebookIcon>&nbsp;Facebook
+                            </IconButton>
+                            <br></br>
+                            <IconButton onClick={() => {window.location.href = 'https://www.instagram.com/washpaoutdoors/';}} edge="start" className={classes.menuButton} color="inherit" aria-label="About Us">
+                                <InstagramIcon></InstagramIcon>&nbsp;Instagram
+                            </IconButton>
+                            <br></br>
+                            <IconButton onClick={() => {window.location.href = 'https://washpaoutdoors.wixsite.com/washpa';}} edge="start" className={classes.menuButton} color="inherit" aria-label="About Us">
+                                <WebIcon></WebIcon>&nbsp;Website
+                            </IconButton>
+                    </Grid>
+                </Grid>
+                </Paper>
+            </React.Fragment>
     );
 
 };
